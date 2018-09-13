660ecf3f8a60a9583746a3997eb818d4561a5c63
Chris Pacia <ctpacia@gmail.com>
2018-09-13 13:49:29 -0400

    Rebrand peer package

--- peer/doc.go
+++ peer/doc.go
@@ -3,22 +3,22 @@
 // license that can be found in the LICENSE file.
 
 /*
-Package peer provides a common base for creating and managing Bitcoin network
-peers.
+Package peer provides a common base for creating and managing Bitcoin cash
+network peers.
 
 Overview
 
 This package builds upon the wire package, which provides the fundamental
-primitives necessary to speak the bitcoin wire protocol, in order to simplify
-the process of creating fully functional peers.  In essence, it provides a
-common base for creating concurrent safe fully validating nodes, Simplified
-Payment Verification (SPV) nodes, proxies, etc.
+primitives necessary to speak the bitcoin cash wire protocol, in order to
+simplify the process of creating fully functional peers.  In essence, it
+provides a common base for creating concurrent safe fully validating nodes,
+Simplified Payment Verification (SPV) nodes, proxies, etc.
 
 A quick overview of the major features peer provides are as follows:
 
- - Provides a basic concurrent safe bitcoin peer for handling bitcoin
+ - Provides a basic concurrent safe bitcoin cash peer for handling bitcoin
    communications via the peer-to-peer protocol
- - Full duplex reading and writing of bitcoin protocol messages
+ - Full duplex reading and writing of bitcoin cash protocol messages
  - Automatic handling of the initial handshake process including protocol
    version negotiation
  - Asynchronous message queuing of outbound messages with optional channel for
@@ -28,10 +28,10 @@ A quick overview of the major features peer provides are as follows:
      incoming connections so they have flexibility to establish connections as
      they see fit (proxies, etc)
    - User agent name and version
-   - Bitcoin network
+   - Bitcoin Cash network
    - Service support signalling (full nodes, bloom filters, etc)
    - Maximum supported protocol version
-   - Ability to register callbacks for handling bitcoin protocol messages
+   - Ability to register callbacks for handling bitcoin cash protocol messages
  - Inventory message batching and send trickling with known inventory detection
    and avoidance
  - Automatic periodic keep-alive pinging and pong responses
@@ -53,9 +53,9 @@ A quick overview of the major features peer provides are as follows:
 Peer Configuration
 
 All peer configuration is handled with the Config struct.  This allows the
-caller to specify things such as the user agent name and version, the bitcoin
+caller to specify things such as the user agent name and version, the bitcoin cash
 network to use, which services it supports, and callbacks to invoke when bitcoin
-messages are received.  See the documentation for each field of the Config
+cash messages are received.  See the documentation for each field of the Config
 struct for more details.
 
 Inbound and Outbound Peers
@@ -76,11 +76,11 @@ cleanup has completed.
 Callbacks
 
 In order to do anything useful with a peer, it is necessary to react to bitcoin
-messages.  This is accomplished by creating an instance of the MessageListeners
+cash messages.  This is accomplished by creating an instance of the MessageListeners
 struct with the callbacks to be invoke specified and setting the Listeners field
 of the Config struct specified when creating a peer to it.
 
-For convenience, a callback hook for all of the currently supported bitcoin
+For convenience, a callback hook for all of the currently supported bitcoin cash
 messages is exposed which receives the peer instance and the concrete message
 type.  In addition, a hook for OnRead is provided so even custom messages types
 for which this package does not directly provide a hook, as long as they
@@ -137,7 +137,7 @@ written, the remote address, user agent, and negotiated protocol version.
 Logging
 
 This package provides extensive logging capabilities through the UseLogger
-function which allows a btclog.Logger to be specified.  For example, logging at
+function which allows a bchlog.Logger to be specified.  For example, logging at
 the debug level provides summaries of every message sent and received, and
 logging at the trace level provides full dumps of parsed messages as well as the
 raw message bytes using a format similar to hexdump -C.
