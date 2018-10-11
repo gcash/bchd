07752f68f12533136ed78ee2045521c863786798
Josh Ellithorpe <quest@mac.com>
2018-10-11 10:31:29 -0700

    Add test for CountSigOps

--- blockchain/validate_test.go
+++ blockchain/validate_test.go
@@ -63,6 +63,47 @@ func TestSequenceLocksActive(t *testing.T) {
 	}
 }
 
+// TestCountSigOps counts the numberof SigOps in a transaction. If OP_CHECKDATASIG is
+// used then it should only be counted after the November hard fork.
+func TestCountSigOps(t *testing.T) {
+	// Invalid tx below, but we are just testing sigop counts.
+	msgTx := wire.MsgTx{
+		Version: 1,
+		TxIn: []*wire.TxIn{
+			{
+				PreviousOutPoint: wire.OutPoint{
+					Hash:  chainhash.Hash{},
+					Index: 0xffffffff,
+				},
+				SignatureScript: []byte{},
+				Sequence:        0xffffffff,
+			},
+		},
+		TxOut: []*wire.TxOut{
+			{
+				Value: 0x12a05f200, // 5000000000
+				PkScript: []byte{
+					0xba, // OP_CHECKDATASIG
+				},
+			},
+		},
+		LockTime: 0,
+	}
+	tx := bchutil.NewTx(&msgTx)
+
+	// Before the Nov fork.
+	sigOps := CountSigOps(tx, false)
+	if sigOps != 0 {
+		t.Fatalf("Incorrect number of SigOps, expected 0 got %v\n", sigOps)
+	}
+
+	// After the Nov fork.
+	sigOps = CountSigOps(tx, true)
+	if sigOps != 1 {
+		t.Fatalf("Incorrect number of SigOps, expected 1 got %v\n", sigOps)
+	}
+}
+
 // TestCheckConnectBlockTemplate tests the CheckConnectBlockTemplate function to
 // ensure it fails.
 func TestCheckConnectBlockTemplate(t *testing.T) {
