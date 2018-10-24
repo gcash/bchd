6ee7df099c92844fa192b2198fe5ce356ca96f60
Josh Ellithorpe <quest@mac.com>
2018-10-24 00:23:33 -0700

    Be consistent, bump to beta2 in bchd

--- version.go
+++ version.go
@@ -22,7 +22,7 @@ const (
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
-	appPreRelease = "beta"
+	appPreRelease = "beta2"
 )
 
 // appBuild is defined as a variable so it can be overridden during the build
