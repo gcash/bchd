67c2dbf935ffa4303eda155b31f752e141fa535a
Josh Ellithorpe <quest@mac.com>
2018-11-21 00:56:37 -0800

    Add tests

--- config_test.go
+++ config_test.go
@@ -13,6 +13,40 @@ var (
 	rpcpassRegexp = regexp.MustCompile("(?m)^rpcpass=.+$")
 )
 
+func TestExcessiveBlockSizeUserAgentComment(t *testing.T) {
+	// Wipe test args.
+	os.Args = []string{"bchd"}
+
+	cfg, _, err := loadConfig()
+	if err != nil {
+		t.Fatalf("Failed to load configuration")
+	}
+
+	if len(cfg.UserAgentComments) != 1 {
+		t.Fatal("Expected EB UserAgentComment")
+	}
+
+	if cfg.UserAgentComments[0] != "EB32.0" {
+		t.Fatal("Expected UserAgentComments to contain EB32.0")
+	}
+
+	// Custom excessive block size.
+	os.Args = []string{"bchd", "--excessiveblocksize=64000000"}
+
+	cfg, _, err = loadConfig()
+	if err != nil {
+		t.Fatalf("Failed to load configuration")
+	}
+
+	if len(cfg.UserAgentComments) != 1 {
+		t.Fatal("Expected EB UserAgentComment")
+	}
+
+	if cfg.UserAgentComments[0] != "EB64.0" {
+		t.Fatal("Expected UserAgentComments to contain EB64.0")
+	}
+}
+
 func TestCreateDefaultConfigFile(t *testing.T) {
 	// Setup a temporary directory
 	tmpDir, err := ioutil.TempDir("", "bchd")
