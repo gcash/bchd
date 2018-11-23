91ca33005dc422ecb7ed8e2dab42e4bb8c93d1f8
Josh Ellithorpe <quest@mac.com>
2018-11-22 17:58:49 -0800

    [Feature] Add EB to UserAgentComments (#151)
    * Add EB to UserAgentComments
    
    * Add tests
    
    * make spec mismatches easier to fix (#152)
    
    * Fix Fatal/Fatalf nit

--- config_test.go
+++ config_test.go
@@ -13,6 +13,44 @@ var (
 	rpcpassRegexp = regexp.MustCompile("(?m)^rpcpass=.+$")
 )
 
+func TestExcessiveBlockSizeUserAgentComment(t *testing.T) {
+	// Wipe test args.
+	os.Args = []string{"bchd"}
+
+	cfg, _, err := loadConfig()
+	if err != nil {
+		t.Fatal("Failed to load configuration")
+	}
+
+	if len(cfg.UserAgentComments) != 1 {
+		t.Fatal("Expected EB UserAgentComment")
+	}
+
+	uac := cfg.UserAgentComments[0]
+	uacExpected := "EB32.0"
+	if uac != uacExpected {
+		t.Fatalf("Expected UserAgentComments to contain %s but got %s", uacExpected, uac)
+	}
+
+	// Custom excessive block size.
+	os.Args = []string{"bchd", "--excessiveblocksize=64000000"}
+
+	cfg, _, err = loadConfig()
+	if err != nil {
+		t.Fatal("Failed to load configuration")
+	}
+
+	if len(cfg.UserAgentComments) != 1 {
+		t.Fatal("Expected EB UserAgentComment")
+	}
+
+	uac = cfg.UserAgentComments[0]
+	uacExpected = "EB64.0"
+	if uac != uacExpected {
+		t.Fatalf("Expected UserAgentComments to contain %s but got %s", uacExpected, uac)
+	}
+}
+
 func TestCreateDefaultConfigFile(t *testing.T) {
 	// Setup a temporary directory
 	tmpDir, err := ioutil.TempDir("", "bchd")
