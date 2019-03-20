4a9f955344e6b92294369d808e81708e98a383d0
Chris Pacia <chris@ob1.io>
2019-03-20 00:20:02 -0400

    Add deadline to GetCFMempool request

--- peer/peer.go
+++ peer/peer.go
@@ -1171,6 +1171,10 @@ func (p *Peer) maybeAddDeadline(pendingResponses map[string]time.Time, msgCmd st
 		// Expects an inv message.
 		pendingResponses[wire.CmdInv] = deadline
 
+	case wire.CmdGetCFMempool:
+		// Expects a cfmempool message.
+		pendingResponses[wire.CmdCFMempool] = deadline
+
 	case wire.CmdGetData:
 		// Expects a block, merkleblock, tx, or notfound message.
 		pendingResponses[wire.CmdBlock] = deadline
