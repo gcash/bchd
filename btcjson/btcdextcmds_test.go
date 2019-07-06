decred/dcrd

rpcserver: add batched request support (json 2.0)

dcrjson/btcdextcmds_test.go
@@ -142,7 +142,7 @@ func TestBtcdExtCmds(t *testing.T) {
 	for i, test := range tests {
 		// Marshal the command as created by the new static command
 		// creation function.
-		marshalled, err := dcrjson.MarshalCmd(testID, test.staticCmd())
+		marshalled, err := dcrjson.MarshalCmd("1.0", testID, test.staticCmd())
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)
@@ -166,7 +166,7 @@ func TestBtcdExtCmds(t *testing.T) {
 
 		// Marshal the command as created by the generic new command
 		// creation function.
-		marshalled, err = dcrjson.MarshalCmd(testID, cmd)
+		marshalled, err = dcrjson.MarshalCmd("1.0", testID, cmd)
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)

dcrjson/chainsvrcmds_test.go
@@ -983,7 +983,7 @@ func TestChainSvrCmds(t *testing.T) {
 	for i, test := range tests {
 		// Marshal the command as created by the new static command
 		// creation function.
-		marshalled, err := dcrjson.MarshalCmd(testID, test.staticCmd())
+		marshalled, err := dcrjson.MarshalCmd("1.0", testID, test.staticCmd())
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)
@@ -1008,7 +1008,7 @@ func TestChainSvrCmds(t *testing.T) {
 
 		// Marshal the command as created by the generic new command
 		// creation function.
-		marshalled, err = dcrjson.MarshalCmd(testID, cmd)
+		marshalled, err = dcrjson.MarshalCmd("1.0", testID, cmd)
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)

dcrjson/chainsvrwscmds_test.go
@@ -163,7 +163,7 @@ func TestChainSvrWsCmds(t *testing.T) {
 	for i, test := range tests {
 		// Marshal the command as created by the new static command
 		// creation function.
-		marshalled, err := dcrjson.MarshalCmd(testID, test.staticCmd())
+		marshalled, err := dcrjson.MarshalCmd("1.0", testID, test.staticCmd())
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)
@@ -187,7 +187,7 @@ func TestChainSvrWsCmds(t *testing.T) {
 
 		// Marshal the command as created by the generic new command
 		// creation function.
-		marshalled, err = dcrjson.MarshalCmd(testID, cmd)
+		marshalled, err = dcrjson.MarshalCmd("1.0", testID, cmd)
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)

dcrjson/chainsvrwsntfns_test.go
@@ -119,7 +119,7 @@ func TestChainSvrWsNtfns(t *testing.T) {
 	for i, test := range tests {
 		// Marshal the notification as created by the new static
 		// creation function.  The ID is nil for notifications.
-		marshalled, err := dcrjson.MarshalCmd(nil, test.staticNtfn())
+		marshalled, err := dcrjson.MarshalCmd("1.0", nil, test.staticNtfn())
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)
@@ -144,7 +144,7 @@ func TestChainSvrWsNtfns(t *testing.T) {
 		// Marshal the notification as created by the generic new
 		// notification creation function.    The ID is nil for
 		// notifications.
-		marshalled, err = dcrjson.MarshalCmd(nil, cmd)
+		marshalled, err = dcrjson.MarshalCmd("1.0", nil, cmd)
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)

dcrjson/jsonrpc_test.go
@@ -16,7 +16,6 @@ import (
 // TestIsValidIDType ensures the IsValidIDType function behaves as expected.
 func TestIsValidIDType(t *testing.T) {
 	t.Parallel()
-
 	tests := []struct {
 		name    string
 		id      interface{}
@@ -57,7 +56,6 @@ func TestIsValidIDType(t *testing.T) {
 // TestMarshalResponse ensures the MarshalResponse function works as expected.
 func TestMarshalResponse(t *testing.T) {
 	t.Parallel()
-
 	testID := 1
 	tests := []struct {
 		name     string
@@ -69,7 +67,7 @@ func TestMarshalResponse(t *testing.T) {
 			name:     "ordinary bool result with no error",
 			result:   true,
 			jsonErr:  nil,
-			expected: []byte(`{"result":true,"error":null,"id":1}`),
+			expected: []byte(`{"jsonrpc":"1.0","result":true,"error":null,"id":1}`),
 		},
 		{
 			name:   "result with error",
@@ -77,14 +75,14 @@ func TestMarshalResponse(t *testing.T) {
 			jsonErr: func() *dcrjson.RPCError {
 				return dcrjson.NewRPCError(dcrjson.ErrRPCBlockNotFound, "123 not found")
 			}(),
-			expected: []byte(`{"result":null,"error":{"code":-5,"message":"123 not found"},"id":1}`),
+			expected: []byte(`{"jsonrpc":"1.0","result":null,"error":{"code":-5,"message":"123 not found"},"id":1}`),
 		},
 	}
 
 	t.Logf("Running %d tests", len(tests))
 	for i, test := range tests {
 		_, _ = i, test
-		marshalled, err := dcrjson.MarshalResponse(testID, test.result, test.jsonErr)
+		marshalled, err := dcrjson.MarshalResponse("1.0", testID, test.result, test.jsonErr)
 		if err != nil {
 			t.Errorf("Test #%d (%s) unexpected error: %v", i,
 				test.name, err)
@@ -102,10 +100,9 @@ func TestMarshalResponse(t *testing.T) {
 // TestMiscErrors tests a few error conditions not covered elsewhere.
 func TestMiscErrors(t *testing.T) {
 	t.Parallel()
-
 	// Force an error in NewRequest by giving it a parameter type that is
 	// not supported.
-	_, err := dcrjson.NewRequest(nil, "test", []interface{}{make(chan int)})
+	_, err := dcrjson.NewRequest("1.0", nil, "test", []interface{}{make(chan int)})
 	if err == nil {
 		t.Error("NewRequest: did not receive error")
 		return
@@ -114,7 +111,7 @@ func TestMiscErrors(t *testing.T) {
 	// Force an error in MarshalResponse by giving it an id type that is not
 	// supported.
 	wantErr := dcrjson.Error{Code: dcrjson.ErrInvalidType}
-	_, err = dcrjson.MarshalResponse(make(chan int), nil, nil)
+	_, err = dcrjson.MarshalResponse("", make(chan int), nil, nil)
 	if jerr, ok := err.(dcrjson.Error); !ok || jerr.Code != wantErr.Code {
 		t.Errorf("MarshalResult: did not receive expected error - got "+
 			"%v (%[1]T), want %v (%[2]T)", err, wantErr)
@@ -123,7 +120,7 @@ func TestMiscErrors(t *testing.T) {
 
 	// Force an error in MarshalResponse by giving it a result type that
 	// can't be marshalled.
-	_, err = dcrjson.MarshalResponse(1, make(chan int), nil)
+	_, err = dcrjson.MarshalResponse("1.0", 1, make(chan int), nil)
 	if _, ok := err.(*json.UnsupportedTypeError); !ok {
 		wantErr := &json.UnsupportedTypeError{}
 		t.Errorf("MarshalResult: did not receive expected error - got "+
@@ -135,7 +132,6 @@ func TestMiscErrors(t *testing.T) {
 // TestRPCError tests the error output for the RPCError type.
 func TestRPCError(t *testing.T) {
 	t.Parallel()
-
 	tests := []struct {
 		in   *dcrjson.RPCError
 		want string

rpcclient/infrastructure.go
@@ -881,7 +881,7 @@ func (c *Client) sendCmd(cmd interface{}) chan *response {
 
 	// Marshal the command.
 	id := c.NextID()
-	marshalledJSON, err := dcrjson.MarshalCmd(id, cmd)
+	marshalledJSON, err := dcrjson.MarshalCmd("1.0", id, cmd)
 	if err != nil {
 		return newFutureError(err)
 	}

gcash/bchd

btcjson/btcdextcmds_test.go
@@ -193,7 +193,7 @@ func TestBtcdExtCmds(t *testing.T) {
 	for i, test := range tests {
 		// Marshal the command as created by the new static command
 		// creation function.
-		marshalled, err := btcjson.MarshalCmd(testID, test.staticCmd())
+		marshalled, err := btcjson.MarshalCmd("1.0", testID, test.staticCmd())
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)
@@ -217,7 +217,7 @@ func TestBtcdExtCmds(t *testing.T) {
 
 		// Marshal the command as created by the generic new command
 		// creation function.
-		marshalled, err = btcjson.MarshalCmd(testID, cmd)
+		marshalled, err = btcjson.MarshalCmd("1.0", testID, cmd)
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)

btcjson/chainsvrcmds_test.go
@@ -1103,7 +1103,7 @@ func TestChainSvrCmds(t *testing.T) {
 	for i, test := range tests {
 		// Marshal the command as created by the new static command
 		// creation function.
-		marshalled, err := btcjson.MarshalCmd(testID, test.staticCmd())
+		marshalled, err := btcjson.MarshalCmd("1.0", testID, test.staticCmd())
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)
@@ -1128,7 +1128,7 @@ func TestChainSvrCmds(t *testing.T) {
 
 		// Marshal the command as created by the generic new command
 		// creation function.
-		marshalled, err = btcjson.MarshalCmd(testID, cmd)
+		marshalled, err = btcjson.MarshalCmd("1.0", testID, cmd)
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)

btcjson/chainsvrwscmds_test.go
@@ -233,7 +233,7 @@ func TestChainSvrWsCmds(t *testing.T) {
 	for i, test := range tests {
 		// Marshal the command as created by the new static command
 		// creation function.
-		marshalled, err := btcjson.MarshalCmd(testID, test.staticCmd())
+		marshalled, err := btcjson.MarshalCmd("1.0", testID, test.staticCmd())
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)
@@ -257,7 +257,7 @@ func TestChainSvrWsCmds(t *testing.T) {
 
 		// Marshal the command as created by the generic new command
 		// creation function.
-		marshalled, err = btcjson.MarshalCmd(testID, cmd)
+		marshalled, err = btcjson.MarshalCmd("1.0", testID, cmd)
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)

btcjson/chainsvrwsntfns_test.go
@@ -231,7 +231,7 @@ func TestChainSvrWsNtfns(t *testing.T) {
 	for i, test := range tests {
 		// Marshal the notification as created by the new static
 		// creation function.  The ID is nil for notifications.
-		marshalled, err := btcjson.MarshalCmd(nil, test.staticNtfn())
+		marshalled, err := btcjson.MarshalCmd("1.0", nil, test.staticNtfn())
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)
@@ -256,7 +256,7 @@ func TestChainSvrWsNtfns(t *testing.T) {
 		// Marshal the notification as created by the generic new
 		// notification creation function.    The ID is nil for
 		// notifications.
-		marshalled, err = btcjson.MarshalCmd(nil, cmd)
+		marshalled, err = btcjson.MarshalCmd("1.0", nil, cmd)
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)

btcjson/jsonrpc_test.go
@@ -15,7 +15,6 @@ import (
 // TestIsValidIDType ensures the IsValidIDType function behaves as expected.
 func TestIsValidIDType(t *testing.T) {
 	t.Parallel()
-
 	tests := []struct {
 		name    string
 		id      interface{}
@@ -56,7 +55,6 @@ func TestIsValidIDType(t *testing.T) {
 // TestMarshalResponse ensures the MarshalResponse function works as expected.
 func TestMarshalResponse(t *testing.T) {
 	t.Parallel()
-
 	testID := 1
 	tests := []struct {
 		name     string
@@ -68,7 +66,7 @@ func TestMarshalResponse(t *testing.T) {
 			name:     "ordinary bool result with no error",
 			result:   true,
 			jsonErr:  nil,
-			expected: []byte(`{"result":true,"error":null,"id":1}`),
+			expected: []byte(`{"jsonrpc":"1.0","result":true,"error":null,"id":1}`),
 		},
 		{
 			name:   "result with error",
@@ -76,14 +74,14 @@ func TestMarshalResponse(t *testing.T) {
 			jsonErr: func() *btcjson.RPCError {
 				return btcjson.NewRPCError(btcjson.ErrRPCBlockNotFound, "123 not found")
 			}(),
-			expected: []byte(`{"result":null,"error":{"code":-5,"message":"123 not found"},"id":1}`),
+			expected: []byte(`{"jsonrpc":"1.0","result":null,"error":{"code":-5,"message":"123 not found"},"id":1}`),
 		},
 	}
 
 	t.Logf("Running %d tests", len(tests))
 	for i, test := range tests {
 		_, _ = i, test
-		marshalled, err := btcjson.MarshalResponse(testID, test.result, test.jsonErr)
+		marshalled, err := btcjson.MarshalResponse("1.0", testID, test.result, test.jsonErr)
 		if err != nil {
 			t.Errorf("Test #%d (%s) unexpected error: %v", i,
 				test.name, err)
@@ -101,10 +99,9 @@ func TestMarshalResponse(t *testing.T) {
 // TestMiscErrors tests a few error conditions not covered elsewhere.
 func TestMiscErrors(t *testing.T) {
 	t.Parallel()
-
 	// Force an error in NewRequest by giving it a parameter type that is
 	// not supported.
-	_, err := btcjson.NewRequest(nil, "test", []interface{}{make(chan int)})
+	_, err := btcjson.NewRequest("1.0", nil, "test", []interface{}{make(chan int)})
 	if err == nil {
 		t.Error("NewRequest: did not receive error")
 		return
@@ -113,7 +110,7 @@ func TestMiscErrors(t *testing.T) {
 	// Force an error in MarshalResponse by giving it an id type that is not
 	// supported.
 	wantErr := btcjson.Error{ErrorCode: btcjson.ErrInvalidType}
-	_, err = btcjson.MarshalResponse(make(chan int), nil, nil)
+	_, err = btcjson.MarshalResponse("", make(chan int), nil, nil)
 	if jerr, ok := err.(btcjson.Error); !ok || jerr.ErrorCode != wantErr.ErrorCode {
 		t.Errorf("MarshalResult: did not receive expected error - got "+
 			"%v (%[1]T), want %v (%[2]T)", err, wantErr)
@@ -122,7 +119,7 @@ func TestMiscErrors(t *testing.T) {
 
 	// Force an error in MarshalResponse by giving it a result type that
 	// can't be marshalled.
-	_, err = btcjson.MarshalResponse(1, make(chan int), nil)
+	_, err = btcjson.MarshalResponse("1.0", 1, make(chan int), nil)
 	if _, ok := err.(*json.UnsupportedTypeError); !ok {
 		wantErr := &json.UnsupportedTypeError{}
 		t.Errorf("MarshalResult: did not receive expected error - got "+
@@ -134,7 +131,6 @@ func TestMiscErrors(t *testing.T) {
 // TestRPCError tests the error output for the RPCError type.
 func TestRPCError(t *testing.T) {
 	t.Parallel()
-
 	tests := []struct {
 		in   *btcjson.RPCError
 		want string

rpcclient/infrastructure.go
@@ -873,7 +873,7 @@ func (c *Client) sendCmd(cmd interface{}) chan *response {
 
 	// Marshal the command.
 	id := c.NextID()
-	marshalledJSON, err := btcjson.MarshalCmd(id, cmd)
+	marshalledJSON, err := btcjson.MarshalCmd("1.0", id, cmd)
 	if err != nil {
 		return newFutureError(err)
 	}