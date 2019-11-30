7d3b4179836b50bbe20cdfd679e5cce6b867cb21
Chris Pacia <chris@ob1.io>
2019-11-30 12:30:26 -0500

    Check for nil req in SubscribeTransactionStream (#324)

--- bchrpc/server.go
+++ bchrpc/server.go
@@ -1282,6 +1282,9 @@ func (s *GrpcServer) SubscribeTransactionStream(stream pb.Bchrpc_SubscribeTransa
 	for {
 		select {
 		case req := <-requests:
+			if req == nil {
+				return nil
+			}
 			includeMempool = req.IncludeMempool
 			includeBlocks = req.IncludeInBlock
 			serializeTx = req.SerializeTx
