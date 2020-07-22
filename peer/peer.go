1f9a429579af7cc256f4521605243e45a6c5564e
quest <quest@mac.com>
2020-07-21 23:18:09 -0700

    peer: prevent last block height going backwards

--- peer/peer.go
+++ peer/peer.go
@@ -530,10 +530,15 @@ func (p *Peer) String() string {
 // This function is safe for concurrent access.
 func (p *Peer) UpdateLastBlockHeight(newHeight int32) {
 	p.statsMtx.Lock()
+	defer p.statsMtx.Unlock()
+
+	if newHeight <= p.lastBlock {
+		return
+	}
+
 	log.Tracef("Updating last block height of peer %v from %v to %v",
 		p.addr, p.lastBlock, newHeight)
 	p.lastBlock = newHeight
-	p.statsMtx.Unlock()
 }
 
 // UpdateLastAnnouncedBlock updates meta-data about the last block hash this
