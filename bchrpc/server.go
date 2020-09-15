9bc5c821627ec97ce1d4daa0404f3e7660e46123
James Cramer <jcramer@simpleledger.io>
2020-09-15 16:59:27 -0400

    refactor use of DecodeAddress with gRPC server
    The goslp version of this method supports SLP address format

--- bchrpc/server.go
+++ bchrpc/server.go
@@ -796,11 +796,7 @@ func (s *GrpcServer) GetAddressTransactions(ctx context.Context, req *pb.GetAddr
 	}
 
 	// Attempt to decode the supplied address.
-	addr, err := bchutil.DecodeAddress(req.Address, s.chainParams)
-	if err != nil {
-		addr, err = goslp.DecodeAddress(req.Address, s.chainParams)
-		addr, err = goslp.ConvertSlpToCashAddress(addr, s.chainParams)
-	}
+	addr, err := goslp.DecodeAddress(req.Address, s.chainParams)
 
 	startHeight := int32(0)
 	if len(req.GetHash()) == 0 {
@@ -882,11 +878,7 @@ func (s *GrpcServer) GetRawAddressTransactions(ctx context.Context, req *pb.GetR
 	}
 
 	// Attempt to decode the supplied address.
-	addr, err := bchutil.DecodeAddress(req.Address, s.chainParams)
-	if err != nil {
-		addr, err = goslp.DecodeAddress(req.Address, s.chainParams)
-		addr, err = goslp.ConvertSlpToCashAddress(addr, s.chainParams)
-	}
+	addr, err := goslp.DecodeAddress(req.Address, s.chainParams)
 
 	startHeight := int32(0)
 	if len(req.GetHash()) == 0 {
@@ -937,11 +929,8 @@ func (s *GrpcServer) GetAddressUnspentOutputs(ctx context.Context, req *pb.GetAd
 	}
 
 	// Attempt to decode the supplied address.
-	addr, err := bchutil.DecodeAddress(req.Address, s.chainParams)
-	if err != nil {
-		addr, err = goslp.DecodeAddress(req.Address, s.chainParams)
-		addr, err = goslp.ConvertSlpToCashAddress(addr, s.chainParams)
-	}
+	addr, err := goslp.DecodeAddress(req.Address, s.chainParams)
+
 	tokenMetadataSet := make(map[chainhash.Hash]struct{})
 	checkTxOutputs := func(tx *wire.MsgTx) ([]*pb.UnspentOutput, error) {
 		txHash := tx.TxHash()
