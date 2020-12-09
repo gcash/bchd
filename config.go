e5c8ab2341d0f2e252ae2099f02499c35c81cb55
quest <quest@mac.com>
2020-12-08 19:27:15 -1000

    Print error and exit if indexing and running a pruned node

--- config.go
+++ config.go
@@ -613,6 +613,15 @@ func loadConfig() (*config, []string, error) {
 		return nil, nil, err
 	}
 
+	// Indexing doesn't work with a pruned blockchain.
+	if (cfg.TxIndex || cfg.AddrIndex) && cfg.Prune {
+		str := "%s: txindex and addrindex can not be used with a pruned blockchain."
+		err := fmt.Errorf(str, funcName)
+		fmt.Fprintln(os.Stderr, err)
+		fmt.Fprintln(os.Stderr, usageMessage)
+		return nil, nil, err
+	}
+
 	// Indexing also doesn't work with fast sync as the indexes will not go
 	// back to genesis.
 	if (cfg.TxIndex || cfg.AddrIndex) && cfg.FastSync {
