ce1cd27f127693a98ee22241da775c3bbcaeac4d
James Cramer <jcramer@simpleledger.io>
2021-03-31 15:08:41 -0400

    should call s.wg.Add(1) before starting the goroutine to avoid a race (SA2000 go-staticcheck)

--- bchrpc/server.go
+++ bchrpc/server.go
@@ -204,8 +204,8 @@ func (s *GrpcServer) subscribeEvents() *rpcEventSubscription {
 
 	// Start a queue handler for the subscription so that slow connections don't
 	// hold up faster ones.
+	s.wg.Add(1)
 	go func() {
-		s.wg.Add(1)
 		queueHandler(sub.in, sub.out, s.quit)
 		s.wg.Done()
 	}()
