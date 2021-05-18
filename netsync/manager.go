9b873852a2d481299e6a01a5c9f7e06f64932d34
James Cramer <jcramer@simpleledger.io>
2021-05-17 20:50:13 -0400

    [SyncManager] fix regtest isSyncCandidate check
    Some node software does not advertise full node service
    when in regtest mode.  We only want to check services
    when we're not running in regression test mode.

--- netsync/manager.go
+++ netsync/manager.go
@@ -455,7 +455,14 @@ func (sm *SyncManager) isSyncCandidate(peer *peerpkg.Peer) bool {
 	// Typically a peer is not a candidate for sync if it's not a full node,
 	// however regression test is special in that the regression tool is
 	// not a full node and still needs to be considered a sync candidate.
-	if sm.chainParams == &chaincfg.RegressionNetParams && !sm.regTestSyncAnyHost {
+	if sm.chainParams != &chaincfg.RegressionNetParams {
+		// The peer is not a candidate for sync if it's not a full
+		// node.
+		nodeServices := peer.Services()
+		return nodeServices&wire.SFNodeNetwork == wire.SFNodeNetwork
+	}
+
+	if !sm.regTestSyncAnyHost {
 		// The peer is not a candidate if it's not coming from localhost
 		// or the hostname can't be determined for some reason.
 		host, _, err := net.SplitHostPort(peer.Addr())
@@ -464,13 +471,6 @@ func (sm *SyncManager) isSyncCandidate(peer *peerpkg.Peer) bool {
 		}
 
 		if host != "127.0.0.1" && host != "localhost" {
-			return false
-		}
-	} else {
-		// The peer is not a candidate for sync if it's not a full
-		// node.
-		nodeServices := peer.Services()
-		if nodeServices&wire.SFNodeNetwork != wire.SFNodeNetwork {
 			return false
 		}
 	}
