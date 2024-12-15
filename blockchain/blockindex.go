b71b1a937e4fe9ca80eda0485cf9f410bef2fd1f
OPReturnCode <mask.em@protonmail.com>
2024-12-15 22:20:29 +0000

    Revert linter commenting needed code

--- blockchain/blockindex.go
+++ blockchain/blockindex.go
@@ -162,9 +162,9 @@ func (node *blockNode) Ancestor(height int32) *blockNode {
 	}
 
 	n := node
-	// for ; n != nil && n.height != height; n = n.parent {
+	for ; n != nil && n.height != height; n = n.parent {
 	// Intentionally left blank
-	// }
+	}
 
 	return n
 }
