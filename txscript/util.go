ccedcae68ea6d899e3579ed0e9cde6d89483335f
quest <quest@mac.com>
2025-05-13 15:04:04 -1000

    Gzip tests to dramatically reduce package size

---
+++ txscript/util.go
@@ -0,0 +1,27 @@
+package txscript
+
+import (
+	"compress/gzip"
+	"io"
+	"os"
+)
+
+func ReadGzFile(filename string) ([]byte, error) {
+	fi, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer fi.Close()
+
+	fz, err := gzip.NewReader(fi)
+	if err != nil {
+		return nil, err
+	}
+	defer fz.Close()
+
+	s, err := io.ReadAll(fz)
+	if err != nil {
+		return nil, err
+	}
+	return s, nil
+}
