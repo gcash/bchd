e4e067ac22fcb1989865f9b607f58393a18c7563
curlwget <curlwget@icloud.com>
2025-06-25 02:32:30 +0800

    Remove duplicate word (#577)
    Signed-off-by: curlwget <curlwget@icloud.com>

--- rpcclient/extensions.go
+++ rpcclient/extensions.go
@@ -24,7 +24,7 @@ type FutureDebugLevelResult chan *response
 
 // Receive waits for the response promised by the future and returns the result
 // of setting the debug logging level to the passed level specification or the
-// list of of the available subsystems for the special keyword 'show'.
+// list of the available subsystems for the special keyword 'show'.
 func (r FutureDebugLevelResult) Receive() (string, error) {
 	res, err := receiveFuture(r)
 	if err != nil {
